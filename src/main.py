ab5aa9399e76f1296c53591fbfed655de37593be
Kirill <kdberdyshev@gmail.com>
2024-05-25 23:49:13 +0300

    Add main code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
- 
+import asyncio
+import logging
+import sys
+from aiogram.enums import ParseMode
+from os import getenv
+from aiogram import Bot
+from aiogram import Dispatcher
+from dotenv import load_dotenv
+
+load_dotenv()
+
+dp = Dispatcher()
+
+async def main():
+    bot = Bot(token=getenv('BOT_TOKEN'), parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
